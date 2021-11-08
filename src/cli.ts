802deadd34eae466e1849e25730fe621349429e8
dmarinsa <dmarinsanchez@gmail.com>
2021-11-08 13:01:21 +0100

    add cli

--- src/cli.ts
+++ src/cli.ts
@@ -1,4 +1,15 @@
-#!/usr/bin/env node
-import { foo } from './main'
+import { Command } from "commander";
+import { init } from './modules/cli/infrastructure/init';
+import { addRobot } from './modules/cli/infrastructure/add-robot';
 
-foo()
+const cli = new Command();
+
+cli
+  .command('init')
+  .description('Initialise our Mars map coordinates expect 2 arguments, if not present it will be 50 50')
+  .action(init);
+
+cli
+  .command('add-robot')
+  .description('Initialise our Mars map coordinates')
+  .action(addRobot);
