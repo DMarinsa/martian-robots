91aa11bc5100ffa3609e5313bf773f1f60f3e10a
dmarinsa <dmarinsanchez@gmail.com>
2021-11-14 21:18:43 +0100

    fix input on init

--- src/modules/cli/infrastructure/init.ts
+++ src/modules/cli/infrastructure/init.ts
@@ -3,11 +3,10 @@ import chalk from "chalk";
 import { log } from "../../../infrastructure/log";
 import { Config } from "../../../infrastructure/persistence/Config";
 
-export const init = (input: string[]) => {
-  const coordinates = input;
-  const x = Number.parseInt(coordinates[0]) || 50;
-  const y = Number.parseInt(coordinates[1]) || 50;
-  const mars = new Mars(x, y);
+export const init = (x: string, y: string) => {
+  const xCoordinate = Number.parseInt(x) || 50;
+  const yCoordinate = Number.parseInt(y) || 50;
+  const mars = new Mars(xCoordinate, yCoordinate);
 
   log(chalk.red.bold(`Mars planet generated with furthest point on [${mars.finalXCoordinate}, ${mars.finalYCoordinate}]`));
 
