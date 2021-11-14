e8b3f21b4b85c043410863d08475dacfb77dc154
dmarinsa <dmarinsanchez@gmail.com>
2021-11-14 21:19:35 +0100

    set commands

--- src/cli.ts
+++ src/cli.ts
@@ -1,15 +1,24 @@
 import { Command } from "commander";
 import { init } from './modules/cli/infrastructure/init';
 import { addRobot } from './modules/cli/infrastructure/add-robot';
+import { clear } from './modules/cli/infrastructure/clear';
 
 const cli = new Command();
 
+
 cli
-  .command('init')
+  .command('init <x> [y]')
   .description('Initialise our Mars map coordinates expect 2 arguments, if not present it will be 50 50')
   .action(init);
 
 cli
-  .command('add-robot')
+  .command('add-robot <x> <y> <orientation> <orders>')
   .description('Initialise our Mars map coordinates')
   .action(addRobot);
+
+cli
+  .command('clear')
+  .description('Clears the whole state')
+  .action(clear);
+
+cli.parse(process.argv);
