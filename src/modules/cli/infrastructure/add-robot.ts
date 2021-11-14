dade089e3eaee52a2446fb1200ea3318f833da03
dmarinsa <dmarinsanchez@gmail.com>
2021-11-14 21:19:12 +0100

    fix add-robot signature

--- src/modules/cli/infrastructure/add-robot.ts
+++ src/modules/cli/infrastructure/add-robot.ts
@@ -1,21 +1,22 @@
 import chalk from "chalk";
 import { log } from "../../../infrastructure/log";
 import { Config } from "../../../infrastructure/persistence/Config";
-import { Orientation } from "../../navigation";
+import { Movement, Orientation } from "../../navigation";
 import { Robot } from "../../robot/domain/Robot";
 
-export const addRobot = (input: string[]) => {
-    const x = Number.parseInt(input[0]) || 50;
-    const y = Number.parseInt(input[1]) || 50;
-    const orientation = input[2] as Orientation;
+export const addRobot = (x: string, y: string, orientation: Orientation, orders: string) => {
+    const xCoordinate = Number.parseInt(x) || 50;
+    const yCoordinate = Number.parseInt(y) || 50;
+    const actions = orders.split('') as Movement[];
 
     let robots = Config.get('robots') as Robot[];
 
     if (!robots) {
       robots = [];
     }
+    
   
-    const robot = new Robot(x, y, orientation);
+    const robot = new Robot(xCoordinate, yCoordinate, orientation, actions);
     robots.push(robot);
     log(chalk.gray.bold(`Robot created`));
   
